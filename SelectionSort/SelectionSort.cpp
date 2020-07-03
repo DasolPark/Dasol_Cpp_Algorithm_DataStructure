bf83fa5eb233c0374f1d398f0325b1d0a368ee7f
Dasol Park <dasol.park615@gmail.com>
2020-07-03 14:42:06 +0900

    [Sort] Selection Sort

---
+++ SelectionSort/SelectionSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  int arr[6] = {0, 5, 3, 2, 4, 1};
+  int i, j;
+  int indexOfMin;
+  int lesser;
+
+  for (i = 1; i <= 4; i++)
+  {
+    indexOfMin = i;
+    for (j = i + 1; j <= 5; j++)
+    {
+      if (arr[j] < arr[indexOfMin])
+      {
+        indexOfMin = j;
+      }
+    }
+    lesser = arr[indexOfMin];
+    arr[indexOfMin] = arr[i];
+    arr[i] = lesser;
+  }
+
+  for (i = 1; i <= 5; i++)
+  {
+    cout << arr[i] << "\n";
+  }
+
+  return 0;
+}
