7880bc8e606eea9ce4e7d77228556ff7204a418c
Dasol Park <dasol.park615@gmail.com>
2020-07-07 17:33:20 +0900

    [Sort] Bubble Sort with C

---
+++ BubbleSort/BubbleSortC.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int arr[10] = {10, 8, 5, 2, 9, 6, 7, 3, 1, 4};
+  int i, j;
+  int lesser;
+
+  for (i = 0; i < 10; i++)
+  {
+    for (j = 0; j < 10 - i - 1; j++)
+    {
+      if (arr[j] > arr[j + 1])
+      {
+        lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+
+  for (i = 0; i < 10; i++)
+    printf("%d ", arr[i]);
+}
